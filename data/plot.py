6ecfbc0762374e304a128aeb4029b9a728a2b323
Roman Rader <roman.rader@gmail.com>
2014-07-20 12:22:27 +0300

    calculate VMR

--- data/plot.py
+++ data/plot.py
@@ -4,36 +4,50 @@ import matplotlib.animation as animation
 import serial
 import threading
 from collections import deque
+import numpy
 
-data = deque(0 for _ in range(5000))
-dataAVG = deque(0 for _ in range(5000))
-dataP = deque(0 for _ in range(5000))
-dataS = deque(0 for _ in range(5000))
+data = deque((0 for _ in range(5000)), maxlen=5000)
+dataAVG = deque((0 for _ in range(5000)), maxlen=5000)
+dataP = deque((0 for _ in range(5000)), maxlen=5000)
+dataS = deque((0 for _ in range(5000)), maxlen=5000)
+dataV = deque((0 for _ in range(5000)), maxlen=5000)
+window = deque((0 for _ in range(500)), maxlen=10)
 avg = 0
 norm = 0
+avg_norm = 0
+i = 0
+var = 0
 
 def addValue(val):
-    global avg, norm
+    global avg, norm, n, avg_norm, i, var
 
     data.append(val)
-    data.popleft()
-    
+
     if avg == 0:
         avg = val
     avg = avg + 0.001 * (val - avg)  # slowly converge to average
     dataAVG.append(avg)
-    dataAVG.popleft()
 
     nval = abs(val - avg)
     norm = norm + 0.1 * (nval - norm)
     dataP.append(norm)
-    dataP.popleft()
+    if i >= 10:
+        window.append(norm)
+        i = 0
+        var = numpy.var(window)
+    i += 1
 
-    n = 0
-    if norm >= 10:
-        n = 1
-    dataS.append(n)
-    dataS.popleft()
+    # confidence = numpy.mean(window) + numpy.var(window)*3  # 3 sigma interval, 99.7%
+    VMR = 0
+    avg_norm = avg_norm + 0.1 * (norm - avg_norm)  # slowly converge to average
+    if avg_norm > 0:
+        VMR = var / avg_norm  # variation coefficient
+    dataV.append(VMR)
+
+    level = 0
+    if VMR >= 1:
+        level = 1
+    dataS.append(level)
 
 
 def msp430():
@@ -53,7 +67,6 @@ def msp430():
         try:
             val = int(ser.readline().strip())
             addValue(val)
-            print(val)
         except ValueError:
             pass
 
@@ -61,15 +74,27 @@ def msp430():
 if __name__ == "__main__":
     threading.Thread(target=msp430).start()
 
-    fig, (p1, p2, p3, p4) = plt.subplots(4, 1)
+    fig = plt.figure()
+    p1 = plt.subplot(3, 2, 1)
     plot_data, = p1.plot(data, animated=True)
-    plot_avg, = p2.plot(data, animated=True)
-    plot_processed, = p3.plot(data, animated=True)
-    plot_signal, = p4.plot(data, animated=True)
+    plot_avg, = p1.plot(data, animated=True)
+    p2 = plt.subplot(3, 2, 3)
+    plot_processed, = p2.plot(data, animated=True)
+    p3 = plt.subplot(3, 2, 5)
+    plot_signal, = p3.plot(data, animated=True)
+    p4 = plt.subplot(1, 2, 2)
+    plot_var, = p4.plot(data, animated=True)
+
+    p1.set_title("raw data and mean")
     p1.set_ylim(0, 0x200)
-    p2.set_ylim(0, 0x200)
-    p3.set_ylim(0, 100)
-    p4.set_ylim(0, 2)
+    p2.set_title("|x_i - mean|")
+    p2.set_ylim(0, 100)
+    p3.set_title("binary signal")
+    p3.set_ylim(0, 2)
+    p4.set_title("VMR")
+    p4.set_ylim(0, 25)
+
+    p4.axhline(y=1.,color='k',ls='dashed')
     def animate(i):
         global data
         plot_data.set_ydata(data)
@@ -80,8 +105,10 @@ if __name__ == "__main__":
         plot_processed.set_xdata(range(len(dataP)))
         plot_signal.set_ydata(dataS)
         plot_signal.set_xdata(range(len(dataS)))
-        return [plot_data, plot_avg, plot_processed, plot_signal]
+        plot_var.set_ydata(dataV)
+        plot_var.set_xdata(range(len(dataV)))
+        return [plot_data, plot_avg, plot_processed, plot_signal, plot_var]
 
     ani = animation.FuncAnimation(fig, animate, range(10000), 
-                                  interval=50, blit=True)
+                                  interval=100, blit=True)
     plt.show()
